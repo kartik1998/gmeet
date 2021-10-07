80a838d46c6a403f1d447a0a9ef09d3bb0f1f266
kartik1998 <kartikrawat61@gmail.com>
2021-10-07 19:20:54 +0530

    Add: logic to compute random string and open meets link

---
+++ index.js
@@ -0,0 +1,16 @@
+const open = require("open");
+
+const baseUrl = "https://g.co/meet";
+
+/**
+ * @returns {url} google meets link
+ */
+function computeGoogleMeetsLink() {
+  let address = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  for (var i = 0; i < 7; i++)
+    address += characters.charAt(Math.floor(Math.random() * characters.length));
+  return `${baseUrl}/${address}`;
+}
+
+open(computeGoogleMeetsLink());
