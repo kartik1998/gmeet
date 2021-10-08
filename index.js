014b79ec40346e086bcc6d4ebff46559ea15a54e
kartik1998 <kartikrawat61@gmail.com>
2021-10-08 15:36:14 +0530

    Update: Make meeting title modifiable via args

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const baseUrl = "https://g.co/meet";
  * @returns {url} google meets link
  */
 function computeGoogleMeetsLink() {
+  if (process.argv.length === 3 && process.argv[2].length >= 1)
+    return `${baseUrl}/${process.argv[2]}`;
   let address = "";
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   for (var i = 0; i < 7; i++)
@@ -15,5 +17,5 @@ function computeGoogleMeetsLink() {
 }
 
 const url = computeGoogleMeetsLink();
-console.log(url)
+console.log(url);
 open(url);
